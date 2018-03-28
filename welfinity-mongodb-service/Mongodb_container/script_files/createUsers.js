31bc9b74e8d9359862efd3db36859081ee593e7a
Cristiano RESSI DI CERVIA <cristiano.ressidicervia@gmail.com>
2018-03-28 15:00:40 +0200

    web user markets and welfinityportal tables

--- welfinity-mongodb-service/Mongodb_container/script_files/createUsers.js
+++ welfinity-mongodb-service/Mongodb_container/script_files/createUsers.js
@@ -7,6 +7,7 @@ db.grantRolesToUser("Welfinity2017!", [ { role: "readWrite", db: "admin" } ]);
 db.grantRolesToUser("Welfinity2017!", [ { role: "readWrite", db: "Pharmacies_list" } ]);
 db.grantRolesToUser("Welfinity2017!", [ { role: "readWrite", db: "Product_Dictionaries_Italy" } ]);
 db.grantRolesToUser("Welfinity2017!", [ { role: "readWrite", db: "markets" } ]);
+db.grantRolesToUser("Welfinity2017!", [ { role: "readWrite", db: "WelfinityPortal" } ]);
 
 
 db = db.getSiblingDB('Pharmacies_list');
@@ -21,3 +22,6 @@ db.createUser(  {    user: "webuser",       pwd: "3ZNnw5T6pWAgeUXx",    roles: [
 db = db.getSiblingDB('markets');
 db.createUser(  {    user: "talendUser",    pwd: "ba+Req6@agu6",    roles: [ { role: "readWrite", db: "markets" }    ]  });
 db.createUser(  {    user: "webuser",       pwd: "3ZNnw5T6pWAgeUXx",    roles: [ { role: "readWrite", db: "markets" }    ]  });
+
+db = db.getSiblingDB('users');
+db.createUser(  {    user: "webuser",       pwd: "3ZNnw5T6pWAgeUXx",    roles: [ { role: "readWrite", db: "WelfinityPortal" }    ]  });
